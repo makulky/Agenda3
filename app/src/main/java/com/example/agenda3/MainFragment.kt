f5e058d7839019793cf94e9dc2d2e745d8f99d39
Miguel <miguelfiguerolaalonso@gmail.com>
2025-03-10 08:24:46 +0100

    Añadido fragmento añadir contacto

--- app/src/main/java/com/example/agenda3/MainFragment.kt
+++ app/src/main/java/com/example/agenda3/MainFragment.kt
@@ -1,18 +1,36 @@
 package com.example.agenda3
 
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.agenda3.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.fab.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_addContactFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
